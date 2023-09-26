d09e936c9dd82fba0a7c5133d1a72925dbcbabbb
iKlotho <umutkahrimanedu@gmail.com>
2023-09-26 23:08:02 +0300

    Add simple lexer

---
+++ json-parser/main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"unicode"
+)
+
+const (
+	BEGIN_ARRAY     = '['
+	BEGIN_OBJECT    = '{'
+	END_ARRAY       = ']'
+	END_OBJECT      = '}'
+	NAME_SEPERATOR  = ':'
+	VALUE_SEPERATOR = ','
+	QUOTATION_MARK  = '"'
+)
+
+type Token struct {
+	text     []byte
+	text_len int
+	kind     string
+}
+
+type Lexer struct {
+	content     []byte
+	content_len int
+	cursor      int
+}
+
+func NewLexer(filename string) *Lexer {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	return &Lexer{
+		content:     content,
+		content_len: len(content),
+		cursor:      0,
+	}
+}
+
+func (l *Lexer) Next() *Token {
+	l.trimLeft()
+	text := []byte{l.content[l.cursor]}
+	token := &Token{
+		text: text,
+		kind: "SYMBOL",
+	}
+	if l.cursor >= l.content_len {
+		return token
+	}
+
+	if l.content[l.cursor] == BEGIN_OBJECT {
+		l.cursor += 1
+		token.kind = "BEGIN_OBJECT"
+		token.text_len = 1
+		return token
+	}
+
+	if l.content[l.cursor] == END_OBJECT {
+		l.cursor += 1
+		token.kind = "END_OBJECT"
+		token.text_len = 1
+		return token
+	}
+
+	if l.content[l.cursor] == QUOTATION_MARK {
+		l.cursor += 1
+		token.kind = "QUOTATION_MARK"
+		token.text_len = 1
+		return token
+	}
+
+	if l.content[l.cursor] == NAME_SEPERATOR {
+		l.cursor += 1
+		token.kind = "NAME_SEPERATOR"
+		token.text_len = 1
+		return token
+	}
+
+	if l.content[l.cursor] == VALUE_SEPERATOR {
+		l.cursor += 1
+		token.kind = "VALUE_SEPERATOR"
+		token.text_len = 1
+		return token
+	}
+
+	if l.content[l.cursor] == BEGIN_ARRAY {
+		l.cursor += 1
+		token.kind = "BEGIN_ARRAY"
+		token.text_len = 1
+		return token
+	}
+
+	if l.content[l.cursor] == END_ARRAY {
+		l.cursor += 1
+		token.kind = "END_ARRAY"
+		token.text_len = 1
+		return token
+	}
+
+	if l.IsAlpha(rune(l.content[l.cursor])) {
+		l.cursor += 1
+		token.kind = "SYMBOL"
+		for {
+			if l.cursor > l.content_len || !l.IsAlpha(rune(l.content[l.cursor])) {
+				break
+			}
+			token.text = append(token.text, l.content[l.cursor])
+			l.cursor += 1
+		}
+		return token
+
+	}
+
+	l.cursor += 1
+	token.kind = "INVALID_TOKEN"
+	token.text_len = 1
+
+	return token
+}
+
+func (l *Lexer) trimLeft() {
+	for l.cursor < l.content_len && unicode.IsSpace(rune(l.content[l.cursor])) {
+		l.cursor++
+	}
+}
+
+func (l *Lexer) IsAlpha(r rune) bool {
+	return unicode.IsLetter(r) || unicode.IsDigit(r) || r == '-'
+}
+
+func main() {
+	filename := os.Args[1]
+	/*
+		RULES
+			BEGIN_QUOTE
+				> SYMBOL | END_QUOTE
+	*/
+
+	lexer := NewLexer(filename)
+	if lexer.content_len == 0 {
+		fmt.Println("Empty file!")
+		os.Exit(1)
+	}
+	fmt.Println("parsing", lexer.content)
+	for {
+		token := lexer.Next()
+		if lexer.cursor == 1 && token.kind != "BEGIN_OBJECT" {
+			fmt.Println("Invalid json!")
+			os.Exit(1)
+		}
+		if token.kind == "END_OBJECT" {
+			break
+		}
+
+		fmt.Println("Token", token.kind, "value", string(token.text))
+	}
+	fmt.Println("Hello lexer", filename)
+}
